1d7ba02325b8a4e5ce8ad9886de4292e3c317f91
Rohit Saini <sketchlearn7@gmail.com>
2020-06-30 00:58:05 +0530

    Code Updated

--- SwiftUI4/ContentView.swift
+++ SwiftUI4/ContentView.swift
@@ -9,8 +9,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject private var signUpViewModel = SignUpViewModel()
     var body: some View {
-        Text("Hello, World!")
+        CustomButton()
+    }
+}
+
+struct CustomButton:View {
+    @State var count = 0
+    var body: some View {
+        ZStack{
+            Button(action: {
+                self.count = self.count + 1
+            }) {
+                Image(systemName: "bell.fill")
+                    .resizable()
+                    .frame(width:30,height: 30)
+            }
+            .padding()
+            .background(Color.gray)
+            .foregroundColor(Color.green)
+            .clipShape(Circle())
+            
+            if count != 0{
+                Text("\(count)")
+                .padding(5)
+                .background(Color.red)
+                .clipShape(Circle())
+                .foregroundColor(Color.white)
+                .offset(x: 17, y: -25)
+            }
+            
+        }
+        
+        
     }
 }
 
--- SwiftUI4/Model/SignUpViewModel.swift
+++ SwiftUI4/Model/SignUpViewModel.swift
@@ -7,3 +7,14 @@
 //
 
 import Foundation
+import Combine
+
+class SignUpViewModel:ObservableObject{
+    //Input
+    @Published var username:String = ""
+    @Published var password:String = ""
+    @Published var passwordAgain:String = ""
+    
+    //Output
+    @Published var isValid:Bool = false
+}
